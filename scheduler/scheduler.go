0535bf56fb25bde268cb135ee9c6e51d0770e03d
Pietro Atzeni <pietrino.atzeni@moneyfarm.com>
2021-12-01 22:30:13 +0100

    Refactored job into scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -4,77 +4,76 @@ import (
 	"fmt"
 	"github.com/beoboo/job-scheduler/library/errors"
 	"github.com/beoboo/job-scheduler/library/helpers"
-	"github.com/beoboo/job-scheduler/library/job"
 	"github.com/beoboo/job-scheduler/library/log"
 	"github.com/beoboo/job-scheduler/library/logsync"
 	"github.com/beoboo/job-scheduler/library/stream"
 )
 
 type Scheduler struct {
-	runJobAsChild bool // For debugging purposes only
-	jobs          map[string]*job.Job
-	m             logsync.Mutex
+	runner string
+	jobs   map[string]*job
+	m      logsync.Mutex
 }
 
 // New creates a scheduler.
-func New(runJobAsChild bool) *Scheduler {
+func New(runner string) *Scheduler {
 	return &Scheduler{
-		runJobAsChild: runJobAsChild,
-		jobs:          make(map[string]*job.Job),
-		m:             logsync.New("Scheduler"),
+		runner: runner,
+		jobs:   make(map[string]*job),
+		m:      logsync.New("Scheduler"),
 	}
 }
 
-// Start runs a new job.Job.
+// Start runs a new job.
 func (s *Scheduler) Start(executable string, args ...string) (string, error) {
 	log.Debugf("Starting executable: \"%s\"\n", helpers.FormatCmdLine(executable, args...))
-	j := job.New()
+	j := newJob()
 
-	// For debugging purposes, bypasses the creation of the spawned child
-	if s.runJobAsChild {
-		err := j.StartChild(executable, args...)
+	// If the executable is the same as the predefined runner, the process has to be isolated
+	if s.runner == executable {
+		err := j.startIsolated(executable, args...)
 		if err != nil {
 			return "", err
 		}
 	} else {
-		err := j.Start(executable, args...)
+		err := j.start(executable, args...)
 		if err != nil {
 			return "", err
 		}
 	}
 
-	log.Debugf("Job ID: %s\n", j.Id())
-	log.Debugf("Status: %s\n", j.Status())
+	log.Debugf("job ID: %s\n", j.id)
+	log.Debugf("Status: %s\n", j.status())
 
 	s.m.WLock("Start")
 	defer s.m.WUnlock("Start")
 
-	s.jobs[j.Id()] = j
-	return j.Id(), nil
+	s.jobs[j.id] = j
+	return j.id, nil
 }
 
-// Stop stops a running job.Job, or an error if the job.Job doesn't exist.
-func (s *Scheduler) Stop(id string) (*job.Status, error) {
+// Stop stops a running job, or an error if the job.job doesn't exist.
+func (s *Scheduler) Stop(id string) (*JobStatus, error) {
 	log.Debugf("Stopping job %s\n", id)
 
-	s.m.RLock("Stop")
-	defer s.m.RUnlock("Stop")
+	s.m.RLock("stop")
+	defer s.m.RUnlock("stop")
 	j, ok := s.jobs[id]
 
 	if !ok {
 		return nil, &errors.NotFoundError{Id: id}
 	}
 
-	err := j.Stop()
+	err := j.stop()
 	if err != nil {
 		return nil, fmt.Errorf("cannot stop job: %s", id)
 	}
 
-	return j.Status(), nil
+	return j.status(), nil
 }
 
-// Status returns the status of a job.Job, or an error if the job.Job doesn't exist.
-func (s *Scheduler) Status(id string) (*job.Status, error) {
+// Status returns the status of a job, or an error if the job.job doesn't exist.
+func (s *Scheduler) Status(id string) (*JobStatus, error) {
 	log.Debugf("Checking status for job \"%s\"\n", id)
 
 	s.m.RLock("Status")
@@ -86,10 +85,10 @@ func (s *Scheduler) Status(id string) (*job.Status, error) {
 		return nil, &errors.NotFoundError{Id: id}
 	}
 
-	return j.Status(), nil
+	return j.status(), nil
 }
 
-// Output returns the stream of the stdout/stderr of a job.Job, or an error if the job.Job doesn't exist.
+// Output returns the stream of the stdout/stderr of a job, or an error if the job.job doesn't exist.
 func (s *Scheduler) Output(id string) (*stream.Stream, error) {
 	log.Debugf("Streaming output for job \"%s\"\n", id)
 
@@ -101,7 +100,7 @@ func (s *Scheduler) Output(id string) (*stream.Stream, error) {
 		return nil, &errors.NotFoundError{Id: id}
 	}
 
-	return j.Output(), nil
+	return j.output(), nil
 }
 
 // Size returns the number of stored jobs.
